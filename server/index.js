d289c494917f5833ef9aa5d9f4e6cd50a0e75260
rbrtknwls <r2knowle@uwaterloo.ca>
2022-01-29 01:36:59 -0500

    hmmmm

--- server/index.js
+++ server/index.js
@@ -6,64 +6,28 @@ require("dotenv").config();
 
 app.use(cors());
 
-let transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    type: "OAuth2",
-    user: process.env.EMAIL,
-    pass: process.env.WORD,
-    clientId: process.env.OAUTH_CLIENTID,
-    clientSecret: process.env.OAUTH_CLIENT_SECRET,
-    refreshToken: process.env.OAUTH_REFRESH_TOKEN,
-  },
-});
-
-transporter.verify((err, success) => {
-  err
-    ? console.log(err)
-    : console.log(`=== Server is ready to take messages: ${success} ===`);
-});
-
-
 const path = require("path");
+const port = process.env.PORT || 3000
 
 
 const start = encodeURI("/");
-
 app.use(start, express.static(path.join(__dirname, "..", "build")));
 app.use(start, express.static("public"));
-app.get(start, (req, res, next) => {
+app.get(start, (req, res) => {
   res.sendFile(path.join(__dirname, "..", "build", "index.html"));
 });
 
-const start = encodeURI("/lobby");
-
-app.use(start, express.static(path.join(__dirname, "..", "build")));
-app.use(start, express.static("public"));
-app.get(start, (req, res, next) => {
+const game = encodeURI("/lobby/*");
+app.get(game,  (req, res) => {
+  console.log("Lobby name");
   res.sendFile(path.join(__dirname, "..", "build", "index.html"));
 });
 
 const sendRoute = encodeURI("/send");
-
 app.post(sendRoute, function (req, res) {
-  const { email, name, message } = req.headers;
-  let mailOptions = {
-    to: process.env.EMAIL,
-    subject: "Message from my Website",
-    text: `From: ${name}\nEmail: ${email}\n\n${message}`,
-  };
 
-  transporter.sendMail(mailOptions, function (err, data) {
-    if (err) {
-      console.log("Error " + err);
-    } else {
-      console.log("Email sent successfully");
-      res.json({ status: "Email sent" });
-    }
-  });
 });
 
-app.listen(process.env.PORT , () => {
-  console.log(`Server is running}`);
+app.listen(port , () => {
+  console.log("Server hosted on " + port);
 });
