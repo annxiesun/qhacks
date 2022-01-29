5ce95db3ecaf09e1f91944390e739e77fb3e5b69
Charismatic_string <C45han@uwaterloo.ca>
2022-01-29 18:04:38 -0500

    game edit

--- server/game.js
+++ server/game.js
@@ -1,8 +1,16 @@
+<<<<<<< Updated upstream
 class game {
     numPlayers
     players
     currPLayer
     currWord
+=======
+
+
+class game {
+    numPlayers
+    players
+>>>>>>> Stashed changes
     animalDict = {
         "cat":1,
         "dog":2,
@@ -19,21 +27,38 @@ class game {
         this.dictionary = dictionary;
     }
 
+<<<<<<< Updated upstream
     checkWord(word) {
         return (word in dictionary && currWord[currWord.length - 1] == word[0])
     }
 
     nextPlayer(player) {
+=======
+    checkWord(word) { 
+        return word in dictionary
+    }
+
+    nextTurn(player, numPlayers) {
+>>>>>>> Stashed changes
         // have a next id variable inside each player instance
         // start i at 1 to exclude the current player
         let nextP = player
         for (let i = 1; i < numPlayers; i++) {
+<<<<<<< Updated upstream
             nextP = nextP.getNext()
             if (nextP.getAlive() == true) {
                 return nextP
             }
         }
         return winGame(player)
+=======
+            nextP = nextP.next()
+            if (nextP.alive() == true) {
+                return nextP
+            }
+        }
+        winGame(player)
+>>>>>>> Stashed changes
     }
     
     winGame(player) {
@@ -41,4 +66,8 @@ class game {
             io.to(p.getId()).emit(`${player.getId()} is the winner!`)
         }
     }
+<<<<<<< Updated upstream
 }
+=======
+}
+>>>>>>> Stashed changes
