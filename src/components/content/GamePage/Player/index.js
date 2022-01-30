02e19d5d5ccfd7d8348381b02ba5e48eac9633aa
Annie Sun <anniesun@Annies-MacBook-Pro.local>
2022-01-30 06:24:19 -0500

    buttons

--- src/components/content/GamePage/Player/index.js
+++ src/components/content/GamePage/Player/index.js
@@ -3,16 +3,16 @@ import styles from './style.module.css';
 import avatarList from '../../../../globals/AvatarList';
 
 
-function Player({ i, name, lives}) {
+function Player({ i, name, lives, active}) {
     let lives_display = [];
     for(let i = 0; i < lives; i++) {
         lives_display.push(<img className={styles.life} src="/resources/heart.png"/>);
     }
 
     return (
-        <div className={styles.container}>
+        <div className={`${styles.container} ${active && styles.active}`}>
                     <p className={styles.p}>{name}</p>
-        <img className={`${styles.pfp} ${(lives <= 0) && styles.dead }`} src={`resources/pfps/${avatarList[i]}`} />
+        <img className={`${styles.pfp} ${active && styles.activeplayer} ${(lives <= 0) && styles.dead }`} src={`resources/pfps/${avatarList[i]}`} />
         <div className={styles.livesContainer}>
         {lives_display}
         </div>
--- src/components/content/GamePage/index.js
+++ src/components/content/GamePage/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import styles from './style.module.css';
 import Player from './Player';
 import { Form, Row, Col } from 'react-bootstrap';
@@ -7,7 +7,10 @@ import FadeIn from 'react-fade-in';
 function Timer({ time }) {
     return (
         <div className={styles.clock}>
-            {time}
+            <div style={{ position: "relative", width: "80px", height: "80px" }}>
+                <img className={styles.clockimage} src={"resources/clock.png"} />
+                <div className={styles.timetext}>{time}</div>
+            </div>
         </div>
     )
 }
@@ -36,29 +39,53 @@ function isCorrect(word) {
 function GamePage() {
     const [wordArr, setWordArr] = useState(["QUIRKY", "QUIRK", "KEEMSTAR"]);
     const [players, setPlayers] = useState(players_default);
+    const [currPlayer, setCurrPlayer] = useState(0);
 
     const [devPlayernum, setDevPlayernum] = useState(0);
     const [time, setTime] = useState(10);
 
+    const nextPlayer = () => {
+        setCurrPlayer((c) => {
+            let cc = c + 1;
+            if (cc >= players.length - 1) {
+                cc = 0;
+            }
+            console.log(players[cc].lives)
+            while (players[cc].lives <= 0) {
+                console.log(cc);
+                cc += 1;
+                console.log(cc);
+                if (cc >= players.length - 1) {
+                    cc = 0;
+                } else {
+                    continue;
+                }
+            }
+            return cc;
+        })
+    }
     const updateLife = (player, num) => {
         setPlayers((arr) => {
-            const newArr = [...arr];
-            newArr[player].lives += num;
+            console.log("hi")
+            const newArr = JSON.parse(JSON.stringify(arr));
+            newArr[player].lives -=1;
             return newArr;
         });
     }
 
     const devLoseLife = () => {
-        updateLife(devPlayernum, -1);
+        console.log(currPlayer, devPlayernum, currPlayer === parseInt(devPlayernum));
+        updateLife(parseInt(devPlayernum), -1);
+        if(currPlayer === parseInt(devPlayernum)) nextPlayer();
     }
 
     const devStartTimer = () => {
         var times = 0;
         var interval = setInterval(() => {
             setTime((t) => {
-                return t-1;
+                return t - 1;
             });
-            times+=1;
+            times += 1;
             if (times >= 10) clearInterval(interval);
         }, 1000);
     }
@@ -80,7 +107,8 @@ function GamePage() {
             //console.log('hi')
             if (isCorrect(e.target.value)) {
                 onUpdateWord(e.target.value);
-            } 
+                nextPlayer();
+            }
             e.target.value = "";
 
         }
@@ -90,9 +118,9 @@ function GamePage() {
         <div className={styles.container}>
             <Timer time={time} />
             <div className={styles.playerContainer}>
-                {players.map((player) => {
+                {players.map((player, i) => {
                     return (
-                        <Player i={player.pic} name={player.username} lives={player.lives} />
+                        <Player active={currPlayer === i} i={player.pic} name={player.username} lives={player.lives} />
                     )
                 })}
             </div>
