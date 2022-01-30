71adb4e59f577d2868a0e1b4382f1d3276dc78b5
rbrtknwls <r2knowle@uwaterloo.ca>
2022-01-30 08:20:13 -0500

    update

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import socketio from "socket.io-client";
 
-export const Socket = socketio.connect("https://vast-hollows-69004.herokuapp.com/");
+export const Socket = socketio.connect("http://www.asdaafsa.space/");
 export const SocketContext = React.createContext();
