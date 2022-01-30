7d1ee7bbdd84ba8d40badd015a9e61c471c976c0
gavinsuns <68662120+gavinsuns@users.noreply.github.com>
2022-01-29 21:14:25 -0500

    remove winGame function from game and edit checkWord

--- server/game.js
+++ server/game.js
@@ -20,25 +20,27 @@ class game {
     }
 
     checkWord(word) {
-        return (word in dictionary && currWord[currWord.length - 1] == word[0])
+        if (word in dictionary && currWord[currWord.length - 1] == word[0]) {
+            currWord = word
+            usedWords.add(word)
+            return true
+        }
+        return false
+    }
+
+    getPlayer() {
+        return currPLayer
     }
 
     nextPlayer(player) {
         // have a next id variable inside each player instance
         // start i at 1 to exclude the current player
         let nextP = player
-        for (let i = 1; i < numPlayers; i++) {
+        for (let i = 0; i < numPlayers; i++) {
             nextP = nextP.getNext()
             if (nextP.getAlive() == true) {
                 return nextP
             }
-        }
-        return winGame(player)
-    }
-    
-    winGame(player) {
-        for (const p of players) {
-            io.to(p.getId()).emit(`${player.getId()} is the winner!`)
         }
     }
 }
